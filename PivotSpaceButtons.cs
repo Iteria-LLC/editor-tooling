df29beb56f1090660b7d082a5ff6886b036daf50
Pete Goodfellow <petethegoat@gmail.com>
2023-12-12 09:51:44 +0000

    Subproject

---
+++ PivotSpaceButtons.cs
@@ -0,0 +1,87 @@
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.Overlays;
+using UnityEditor.Toolbars;
+using System.Collections.Generic;
+
+namespace Iteria.EditorTooling
+{
+	[Overlay(typeof(SceneView), "Pivot & Space Selection", defaultDisplay = true)]
+	public class PivotSpaceButtons : Overlay, ICreateToolbar
+	{
+		IEnumerable<string> ICreateToolbar.toolbarElements => k_ToolbarItems;
+		static readonly string[] k_ToolbarItems = new[]
+		{
+		"Toggle Tool Handle Rotation",
+		"Toggle Tool Handle Position"
+	};
+
+		public override VisualElement CreatePanelContent() => new Label() { text = "Please dock me to a toolbar! I'm your pivot and space controls!" };
+	}
+
+	[EditorToolbarElement("Toggle Tool Handle Rotation", typeof(SceneView))]
+	class GlobalLocalToggle : EditorToolbarToggle
+	{
+		const string global = " Global";
+		const string local = " Local";
+		PivotRotation expectedRotation = PivotRotation.Global;
+
+		public GlobalLocalToggle()
+		{
+			offIcon = EditorGUIUtility.FindTexture("d_ToolHandleGlobal");
+			onIcon = EditorGUIUtility.FindTexture("d_ToolHandleLocal");
+			this.RegisterValueChangedCallback(OnChange);
+			EditorApplication.update += CheckChange;
+
+			text = Tools.pivotRotation == PivotRotation.Local ? local : global;
+		}
+
+		void OnChange(ChangeEvent<bool> e)
+		{
+			text = e.newValue ? local : global;
+			Tools.pivotRotation = e.newValue ? PivotRotation.Local : PivotRotation.Global;
+		}
+
+		void CheckChange()
+		{
+			if(Tools.pivotRotation != expectedRotation)
+			{
+				ToggleValue();
+				expectedRotation = Tools.pivotRotation;
+			}
+		}
+	}
+
+	[EditorToolbarElement("Toggle Tool Handle Position", typeof(SceneView))]
+	class PivotCenterToggle : EditorToolbarToggle
+	{
+		const string pivot = " Pivot";
+		const string center = " Center";
+		PivotMode expectedMode = PivotMode.Center;
+
+		public PivotCenterToggle()
+		{
+			offIcon = EditorGUIUtility.FindTexture("d_ToolHandleCenter");
+			onIcon = EditorGUIUtility.FindTexture("d_ToolHandlePivot");
+			this.RegisterValueChangedCallback(OnChange);
+			EditorApplication.update += CheckChange;
+
+			text = Tools.pivotMode == PivotMode.Pivot ? pivot : center;
+		}
+
+		void OnChange(ChangeEvent<bool> e)
+		{
+			text = e.newValue ? pivot : center;
+			Tools.pivotMode = e.newValue ? PivotMode.Pivot : PivotMode.Center;
+		}
+
+		void CheckChange()
+		{
+			if(Tools.pivotMode != expectedMode)
+			{
+				ToggleValue();
+				expectedMode = Tools.pivotMode;
+			}
+		}
+	}
+}
---
+++ Tools.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using static System.IO.Path;
+
+namespace Iteria.EditorTooling
+{
+	public static class EditorTools
+	{
+		[MenuItem("File/Run Existing Build", priority = 211)]
+		public static void RunExistingBuild()
+		{
+			var path = GetFullPath(Combine(Application.dataPath, "..", "Builds", $"{Application.productName}.exe"));
+			if(System.IO.File.Exists(path))
+				System.Diagnostics.Process.Start(path);
+			else
+				Debug.LogError($"Couldn't find {GetFileName(path)} in {GetDirectoryName(path)}. Aborting.");
+		}
+
+		[MenuItem("Tools/Editor/Rotate 90"), Shortcut("Iteria/Rotate 90", KeyCode.R, ShortcutModifiers.Shift)]
+		public static void Rotate90()
+		{
+			if(EditorWindow.focusedWindow as SceneView == null)
+				return;
+
+			if(Selection.activeTransform != null)
+			{
+				var transforms = Selection.GetTransforms(SelectionMode.TopLevel);
+
+				Vector3 center = Vector3.zero;
+				foreach(var t in transforms)
+					center += t.position;
+				center /= transforms.Length;
+
+				Undo.RecordObjects(transforms, "Rotate 90 Degrees");
+				foreach(var t in transforms)
+				{
+					if(UnityEditor.Tools.pivotMode == PivotMode.Pivot)
+						t.RotateAround(Selection.activeTransform.position, Vector3.up, 90f);
+					else
+						t.RotateAround(center, Vector3.up, 90f);
+				}
+			}
+		}
+	}
+}
