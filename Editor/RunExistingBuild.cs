e5ee346895b2ea7ccff5c243add35bf83725d1fb
Pete Goodfellow <petethegoat@gmail.com>
2024-01-02 11:17:03 +0000

    Update RunExistingBuild.cs
    Use a path specific to the project

--- Editor/RunExistingBuild.cs
+++ Editor/RunExistingBuild.cs
@@ -1,34 +1,35 @@
 using UnityEditor.Build.Reporting;
 using UnityEditor.Build;
 using UnityEditor;
-using static System.IO.Path;
+using UnityEngine;
 
 namespace Iteria.EditorTooling
 {
 	public class Build : IPostprocessBuildWithReport
 	{
-		const string prefsLastBuildLocation = "Iteria_last_build_location";
+		const string lastBuildLocation = "Iteria_last_build_location";
+		static string prefsLastBuildKey => $"{lastBuildLocation}_{Application.productName}";
 		public int callbackOrder => 0;
 
 		public void OnPostprocessBuild(BuildReport report)
 		{
-			EditorPrefs.SetString(prefsLastBuildLocation, report.summary.outputPath);
+            EditorPrefs.SetString(prefsLastBuildKey, report.summary.outputPath);
 		}
 
 		[MenuItem("File/Run Existing Build", priority = 211)]
 		public static void RunExistingBuild()
 		{
-			if(!EditorPrefs.HasKey(prefsLastBuildLocation))
+			if(!EditorPrefs.HasKey(prefsLastBuildKey))
 			{
-				UnityEngine.Debug.LogError("No previous build found. Try making a new build with this package installed.");
+                Debug.LogError("No previous build found. Try making a new build with this package installed.");
 				return;
 			}
 
-			var path = EditorPrefs.GetString(prefsLastBuildLocation);
+			var path = EditorPrefs.GetString(prefsLastBuildKey);
 			if(System.IO.File.Exists(path))
 				System.Diagnostics.Process.Start(path);
 			else
-				UnityEngine.Debug.LogError($"Couldn't find {GetFileName(path)} in {GetDirectoryName(path)}. Aborting.");
+				Debug.LogError($"Couldn't find {System.IO.Path.GetFileName(path)} in {System.IO.Path.GetDirectoryName(path)}. Aborting.");
 		}
 	}
 }
